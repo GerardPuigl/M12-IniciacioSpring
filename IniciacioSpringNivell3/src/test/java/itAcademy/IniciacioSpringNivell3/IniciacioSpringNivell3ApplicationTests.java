f76b1eb82e2ef17bfdd096c30ec2ad1ff67c8007
Gerard <gerardpuig31@hotmail.com>
2021-03-04 23:42:22 +0100

    Fase3 Completada

---
+++ IniciacioSpringNivell3/src/test/java/itAcademy/IniciacioSpringNivell3/IniciacioSpringNivell3ApplicationTests.java
@@ -0,0 +1,13 @@
+package itAcademy.IniciacioSpringNivell3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniciacioSpringNivell3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
