3dcd8617c8122357f4c305286255a4495430244a
Gerard <gerardpuig31@hotmail.com>
2021-03-04 22:44:30 +0100

    Fase 2 Completada

---
+++ IniciacioSpringNivell2/src/test/java/itacademy/IniciacioSpring/IniciacioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package itacademy.IniciacioSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IniciacioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
